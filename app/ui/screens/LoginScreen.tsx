ea2a46e08142b69a5602be7a7ccbf88ad7de551f
D4rkgh057 <danielfuelpaz@outlook.com>
2025-05-22 13:22:18 -0500

    feat: update LoginScreen layout and styling for improved user experience

--- app/ui/screens/LoginScreen.tsx
+++ app/ui/screens/LoginScreen.tsx
@@ -5,18 +5,18 @@ export default function LoginScreen() {
 
   return (
     <View className="flex-1 justify-center items-center p-5 bg-white">
-      <Image source={require('../../../assets/images/ruta593.png')} className="w-280 h-93 mb-12" />
       <Image source={require('../../../assets/images/mountain.png')} className="w-280 h-93 mb-12" />
-      <TextInput className="w-full h-12 border border-gray-300 rounded p-2 mb-2 bg-gray-100" placeholder="Ingresa tu Correo" />
-      <TextInput className="w-full h-12 border border-gray-300 rounded p-2 mb-2 bg-gray-100" placeholder="Ingresa tu Contraseña" secureTextEntry />
+      <Image source={require('../../../assets/images/ruta593.png')} className="w-280 h-93 mb-12" />
+      <TextInput className="w-80 h-14 border border-gray-300 rounded-xl p-4 mb-2 " placeholder="Ingresa tu Correo" />
+      <TextInput className="w-80 h-14 border border-gray-300 rounded-xl p-4 mb-2 " placeholder="Ingresa tu Contraseña" secureTextEntry />
       <TouchableOpacity className="self-end mb-5">
-        <Text className="text-blue-500">¿Olvidaste tu contraseña?</Text>
+        <Text className="text-blue-800">¿Olvidaste tu contraseña?</Text>
       </TouchableOpacity>
-      <TouchableOpacity className="w-full h-12 bg-yellow-400 justify-center items-center rounded mb-5">
+      <TouchableOpacity className="w-56 h-14 bg-yellow-400 justify-center items-center rounded-xl mb-5">
         <Text className="text-black font-bold">Iniciar Sesión</Text>
       </TouchableOpacity>
       <TouchableOpacity className="mt-5">
-        <Text className="text-blue-500">¿Aún no te registras? Crea una Cuenta</Text>
+        <Text className="text-blue-800">¿Aún no te registras? Crea una Cuenta</Text>
       </TouchableOpacity>
     </View>
   );
