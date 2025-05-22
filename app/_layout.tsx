0a09c29aed9fc1f7c10b5d8c93f2fb7b93b46040
D4rkgh057 <danielfuelpaz@outlook.com>
2025-05-22 14:57:50 -0500

    feat: add Inter font loading and update package dependencies for improved functionality

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,17 @@
+import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
 import "../global.css";
 export default function RootLayout() {
+
+   const [fontsLoaded] = useFonts({
+      'Inter': require('../assets/fonts/Inter_18pt-Bold.ttf'),
+      // Agrega más fuentes si es necesario
+    });
+  
+    if (!fontsLoaded) {
+      return null;
+    }
+  
 
   return <Stack screenOptions={
     {
