d0d3c2dc0f9f27a2a9094de3123faf17c7f492f2
D4rkgh057 <danielfuelpaz@outlook.com>
2025-05-21 19:34:59 -0500

    feat: add NativeWind support and custom fonts, update Tailwind config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-
+import "../global.css";
 export default function RootLayout() {
-  return <Stack />;
+
+  const [fontsLoaded] = useFonts({
+    'MyCustomFont-Regular': require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null; // or a loading indicator
+  }
+
+  return <Stack screenOptions={
+    {
+      headerShown: false,
+    }
+  }/>;
 }
